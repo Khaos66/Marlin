dfce692b195acf1f3f3099849c4a5518a8e0a885
Khaos <khaos@khaos-coders.org>
2023-06-06 16:55:33 +0200

    homeing speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1521,10 +1521,10 @@
 #define PROBING_MARGIN 6
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_FEEDRATE (133*60)
+#define XY_PROBE_FEEDRATE (200*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
-#define Z_PROBE_FEEDRATE_FAST (4*60)
+#define Z_PROBE_FEEDRATE_FAST (5*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
 #define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST / 2)
